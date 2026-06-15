016a0e5d88b6e54b1cbd65b2f6f4cbd1d55340a9
Игорь Антипов <igorantivirus@yandex.ru>
2026-06-15 23:03:12 +0700

    Добавлена новая public информация о пользователе 1) Страна 2) ЛДата рождения 3) Дата регистрации

--- src/Protocol/Users.hpp
+++ src/Protocol/Users.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <ctime>
 #include <string>
 #include <unordered_map>
@@ -10,10 +11,33 @@
 // вспомогательные структуры
 namespace protocol::users
 {
+// Страна пользователя. Сериализуется строкой (magic_enum).
+enum class Country : std::uint8_t
+{
+    None, // не указана
+    Ru,   // Россия
+    By,   // Беларусь
+    Kz,   // Казахстан
+    Ua,   // Украина
+    Us,   // США
+    Gb,   // Великобритания
+    De,   // Германия
+    Fr,   // Франция
+    Cn,   // Китай
+    Jp,   // Япония
+    In,   // Индия
+    Br    // Бразилия
+};
+
+// Публичная информация о пользователе. Вся информация о юзере публичная,
+// поэтому здесь же несём дату рождения, страну и время регистрации.
 struct UserDisplayInfo
 {
     std::string username;
     std::string displayname;
+    std::time_t birthDate;    // дата рождения (присылается клиентом, не валидируется)
+    Country country;          // страна пользователя
+    std::time_t registerTime; // время первой регистрации (ставит сервер)
 };
 } // namespace protocol::users
 
@@ -31,12 +55,17 @@ struct MeResponse
     std::string username;
     std::string displayname;
     std::time_t registerTime;
+    std::time_t birthDate;
+    Country country;
 };
 struct UserResponse
 {
     UserId userId;
     std::string username;
     std::string displayname;
+    std::time_t registerTime;
+    std::time_t birthDate;
+    Country country;
 };
 
 struct UsersLoopByExampleResponse
