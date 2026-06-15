e2440f5b2f00c44adaa0d6a4f7ec18af9089c68c
Игорь Антипов <igorantivirus@yandex.ru>
2026-06-15 23:03:28 +0700

    Добавлены поля: дата рождения и страна

--- src/Auth/Types/User.hpp
+++ src/Auth/Types/User.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <ctime>
 #include <string>
 
 #include <Protocol/Types.hpp>
+#include <Protocol/Users.hpp>
 
 struct User
 {
@@ -11,4 +13,6 @@ struct User
     std::string passwordHash;
     std::int64_t registerTime;
     std::string displayeName;
+    std::time_t birthDate;
+    protocol::users::Country country;
 };
