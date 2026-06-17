b9ed7ead3e3425b189242965fbdcddba5d988ca3
Игорь Антипов <igorantivirus@yandex.ru>
2026-06-17 14:59:26 +0700

    Добавлена роль пользователя

--- src/ChatService/Types/RoomWithLastMessage.hpp
+++ src/ChatService/Types/RoomWithLastMessage.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+#include <Protocol/Rooms.hpp>
+
 #include "Message.hpp"
 #include "Room.hpp"
 
@@ -10,4 +12,5 @@ struct RoomWithLastMessage
     Room room;
     Message msg;
     std::uint32_t participantsCount;
+    protocol::rooms::MemberRole role; // роль запрашивающего пользователя в этой комнате
 };
